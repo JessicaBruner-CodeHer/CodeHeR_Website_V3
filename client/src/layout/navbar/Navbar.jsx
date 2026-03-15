29e4c40a7aa0409c73b0b405bfc6ef75fa0c1508
Jessica Bruner <info@codeherllc.com>
2026-03-14 19:33:56 -0700

    file add three

---
+++ client/src/layout/navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import navLinks from "../../assets/constants/navLinks";
+import siteConfig from "../../assets/constants/siteConfig";
+import Button from "../../ui/button/Button";
+import "./navbar.css";
+
+const Navbar = ({ onQuoteClick }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <header className="navbar">
+      <div className="site-container navbar-inner">
+        <div className="navbar-brand">
+          {siteConfig.brand.name}
+        </div>
+
+        <nav className={`navbar-links ${menuOpen ? "is-open" : ""}`}>
+          {navLinks.map((link) => (
+            <a
+              key={link.id}
+              href={link.href}
+              className="navbar-link"
+              onClick={handleLinkClick}
+            >
+              {link.label}
+            </a>
+          ))}
+        </nav>
+
+        <div className="navbar-actions">
+          <Button
+            variant="quote"
+            label={siteConfig.navigation.ctaLabel}
+            onClick={onQuoteClick}
+          />
+
+          <button
+            className="navbar-toggle"
+            onClick={handleToggleMenu}
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggle-bar"></span>
+            <span className="navbar-toggle-bar"></span>
+            <span className="navbar-toggle-bar"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
